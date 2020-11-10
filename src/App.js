4cf887b2453c97f9676400d38839ab455934d85e
Brunno Lorran <brunnolorran.silva@gmail.com>
2020-11-10 14:43:21 -0300

    created button test and altered favicon.ico

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 
 const App = () => {
+  const handleToggle = () => {
+    console.log("Verificado");
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Inicial</h1>
+        <button onClick={() => handleToggle()}>Testar handle</button>
       </header>
     </div>
   );
