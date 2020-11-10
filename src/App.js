6c556d342fe0f0693e3f42b37346808927f440f8
Brunno Lorran <brunnolorran.silva@gmail.com>
2020-11-10 14:59:44 -0300

    created fetch and modified icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 
+import { useState } from "react";
+
 const App = () => {
+  const [user, setUser] = useState("");
+
   const handleToggle = () => {
-    console.log("Verificado");
+    fetch("https://api.github.com/users/brunnolorran").then((response) =>
+      response.json().then((data) => setUser(data.name))
+    );
   };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Inicial</h1>
-        <button onClick={() => handleToggle()}>Testar handle</button>
+        {user}
+        <button onClick={handleToggle}>Testar handle</button>
       </header>
     </div>
   );
